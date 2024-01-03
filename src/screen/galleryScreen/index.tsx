86e04da550b583742d37f3673562c4819c3473dd
shimeeuisuk <euisuk95@gmail.com>
2024-01-03 22:22:47 +0900

    feat: screen boiler plate

---
+++ src/screen/galleryScreen/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const GalleryScreen = () => {
+  return <View></View>;
+};
+
+export default GalleryScreen;
---
+++ src/screen/replyScreen/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ReplyScreen = () => {
+  return <View></View>;
+};
+
+export default ReplyScreen;
