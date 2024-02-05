7089c5cce882d39947d118700eb3e8133f34d3f4
shimeeuisuk <euisuk95@gmail.com>
2024-02-05 17:37:25 +0900

    feat: add TitleBox components

---
+++ src/components/titleBox/index.tsx
@@ -0,0 +1,39 @@
+import React, {SetStateAction, useState} from 'react';
+import {TextInput, View} from 'react-native';
+import {globalStyles} from '../../assets/globalStyles';
+import {Typography} from '@shimeuisuk/simicocobab';
+import {styles} from './styles';
+
+interface Props {
+  title: string;
+  setTitle: React.Dispatch<SetStateAction<string>>;
+}
+
+const TiltleBox = ({title, setTitle}: Props) => {
+  const [isInputTouched, setIsInputTouched] = useState<boolean>(false);
+
+  const date = new Date();
+  const todayDate = date.getDate();
+  const todayMonth = date.getMonth() + 1;
+
+  return (
+    <View style={[styles.container, globalStyles.shadow]}>
+      <Typography
+        fontSize={12}
+        color={'#858585'}
+        style={styles.date}>{`${todayMonth}월 ${todayDate}일`}</Typography>
+
+      <TextInput
+        value={title}
+        onChangeText={text => setTitle(text)}
+        placeholder={isInputTouched ? '' : `Today is?`}
+        style={styles.title}
+        maxLength={11}
+        onFocus={() => setIsInputTouched(true)}
+        onBlur={() => setIsInputTouched(false)}
+      />
+    </View>
+  );
+};
+
+export default TiltleBox;
---
+++ src/components/titleBox/styles.ts
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    marginHorizontal: 45,
+    height: 85,
+    borderRadius: 20,
+    marginTop: 20,
+    paddingTop: 12,
+  },
+  date: {
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginTop: 15,
+  },
+});
