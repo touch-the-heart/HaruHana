f48350dc32819b2946ac2f7544a8e745559d65af
shimeeuisuk <euisuk95@gmail.com>
2024-02-05 17:37:02 +0900

    feat: add HomeScreen

--- src/screen/homeScreen/index.tsx
+++ src/screen/homeScreen/index.tsx
@@ -1,8 +1,49 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {Pressable, ScrollView, View} from 'react-native';
+import Header from '../../components/header';
+import {Typography} from '@shimeuisuk/simicocobab';
+import LeftImageCard from '../../components/imageCard/leftImageCard';
+import RightImageCard from '../../components/imageCard/rightImageCard';
+import TiltleBox from '../../components/titleBox';
+import {styles} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import IconC from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const HomeScreen = () => {
-  return <View></View>;
+  const [title, setTitle] = useState<string>('');
+  const [frameModalVisible, setFrameModalVisible] = useState<boolean>(false);
+
+  const headerDate = (
+    <Typography fontSize={13} style={styles.headerDate}>
+      {'2218일째'}
+    </Typography>
+  );
+
+  return (
+    <ScrollView style={styles.container}>
+      <Header title={'의석 ❤️ 교석'} right={headerDate} />
+
+      <TiltleBox title={title} setTitle={setTitle} />
+
+      <View style={styles.imageContainer}>
+        <LeftImageCard />
+        <RightImageCard />
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <Pressable onPress={() => setFrameModalVisible(!frameModalVisible)}>
+          <IconC name={'collage'} color={'#969696'} size={25} />
+        </Pressable>
+
+        <Pressable style={styles.replyButton}>
+          <Typography fontSize={13} style={styles.replyText} color={'#969696'}>
+            {'댓글'}
+          </Typography>
+          <Icon name={'chevron-right'} color={'#969696'} size={15} />
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default HomeScreen;
---
+++ src/screen/homeScreen/styles.ts
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: '#FFFFFF',
+  },
+  imageContainer: {
+    marginHorizontal: 20,
+    marginTop: 35,
+    marginBottom: 410,
+  },
+  headerDate: {
+    fontWeight: '500',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  replyButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  replyText: {
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+});
