406f31394fee001d04b0be3b20978a0ab6a1145d
shimeeuisuk <euisuk95@gmail.com>
2024-02-15 14:00:12 +0900

    feat: add loginButton content text

---
+++ src/assets/text.ts
@@ -0,0 +1,9 @@
+export const text = {
+  // loginScreen
+  loginButton: ['Kakao로 계속하기', 'Google로 계속하기'],
+  introduction: [
+    '하루에 하나 만큼 늘어나는 우리',
+    '매일 새로운 키워드',
+    '한눈에 확인할 수 있는 달력',
+  ],
+};
