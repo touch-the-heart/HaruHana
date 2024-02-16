83df060f939a286d3e611a0d2dc9be07dfd72e5d
shimeeuisuk <euisuk95@gmail.com>
2024-02-16 17:20:25 +0900

    refactor: add MainStack

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import LoginScreen from './src/screen/loginScreen';
+import MainStack from './src/stack/mainStack';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,13 +19,14 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <LoginScreen />
-    </SafeAreaView>
+    <MainStack>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+      </SafeAreaView>
+    </MainStack>
   );
 }
 
