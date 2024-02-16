099086e085de2a3f0577207eedf38e81ed824469
shimeeuisuk <euisuk95@gmail.com>
2024-02-16 17:18:54 +0900

    refactor: add marginTop style & add press props

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,32 +3,39 @@ import React from 'react';
 import {Pressable, View} from 'react-native';
 import {styles} from './styles';
 import Line from '../line';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 interface Props {
   left?: JSX.Element;
+  leftOnpress?: () => void;
   title?: string;
   titleFontSize?: number;
   right?: JSX.Element;
+  rightOnpress?: () => void;
   border?: boolean;
 }
 
 const Header = ({
   left,
+  leftOnpress,
   title,
   titleFontSize = 16,
   right,
+  rightOnpress,
   border = false,
 }: Props) => {
+  const insets = useSafeAreaInsets();
+
   return (
     <>
-      <View style={styles.container}>
-        {left && <Pressable>{left}</Pressable>}
+      <View style={[styles.container, {marginTop: insets.top}]}>
+        {left && <Pressable onPress={leftOnpress}>{left}</Pressable>}
         {title && (
           <Typography fontSize={titleFontSize} style={styles.title}>
             {title}
           </Typography>
         )}
-        {right && <Pressable>{right}</Pressable>}
+        {right && <Pressable onPress={rightOnpress}>{right}</Pressable>}
       </View>
 
       {border && <Line />}
